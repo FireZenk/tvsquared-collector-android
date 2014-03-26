8ca7fe960811fcdae1376d44e7b337a318f76aea
Andrew de Quincey <adq@lidskialf.net>
2014-03-26 13:22:27 +0000

    Iniitial checkin of tracker code

---
+++ src/com/tvsquared/TVSquaredCollector.java
@@ -0,0 +1,199 @@
+package com.tvsquared;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.Random;
+import java.util.UUID;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
+
+public class TVSquaredCollector {
+
+    private String hostname = null;
+    private String siteid = null;
+    private boolean secure = false;
+    private String visitorid = null;
+
+    private String userId = null;
+    private Random random = new Random(new Date().getTime());
+    private static final boolean IS_ICS_OR_LATER = Build.VERSION.SDK_INT >= 14; /*Build.VERSION_CODES.ICE_CREAM_SANDWICH; */
+    private HttpHost proxy = null;
+
+    public TVSquaredCollector(Activity activity, String hostname, String siteid) throws NoSuchAlgorithmException {
+        this(activity, hostname, siteid, false);
+    }
+
+    public TVSquaredCollector(Activity activity, String hostname, String siteid, boolean secure) throws NoSuchAlgorithmException {
+        this.hostname = hostname;
+        this.siteid = siteid;
+        this.secure = secure;
+
+        this.visitorid = this.getVisitorId(activity);
+        this.proxy = this.getProxy(activity);
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void track() {
+        this.track(null, null, null, 0, null);
+    }
+
+    public void track(String actionname, String product, String promocode, float revenue, String orderid) {
+        new Thread(new AsyncTrack(this, actionname, product, promocode, revenue, orderid)).start();
+    }
+
+    private String getVisitorId(Activity activity)
+            throws NoSuchAlgorithmException {
+        String prefname = "visitor" + this.siteid;
+
+        SharedPreferences settings = activity.getSharedPreferences("TVSquaredTracker", 0);
+        String visitor = settings.getString(prefname, null);
+        if (visitor == null) {
+            visitor = this.md5(UUID.randomUUID().toString()).substring(0, 16);
+            settings.edit().putString(prefname, visitor).commit();
+        }
+        return visitor;
+    }
+
+    private void appendSessionDetails(Uri.Builder builder)
+            throws JSONException {
+        JSONObject v5 = new JSONObject();
+        v5.put("medium", "app");
+        v5.put("dev", "android");
+        if (this.userId != null)
+            v5.put("user", this.userId);
+
+        JSONArray custom5 = new JSONArray();
+        custom5.put("session");
+        custom5.put(v5.toString());
+
+        JSONObject cvar = new JSONObject();
+        cvar.put("5", custom5);
+
+        builder.appendQueryParameter("_cvar", cvar.toString());
+    }
+
+    private void appendActionDetails(Uri.Builder builder, String actionname,
+                                     String product, String promocode, float revenue, String orderid)
+                                             throws JSONException {
+        JSONObject v5 = new JSONObject();
+        v5.put("prod", product);
+        v5.put("promo", promocode);
+        v5.put("rev", revenue);
+        v5.put("id", orderid);
+
+        JSONArray custom5 = new JSONArray();
+        custom5.put(actionname);
+        custom5.put(v5.toString());
+
+        JSONObject cvar = new JSONObject();
+        cvar.put("5", custom5);
+
+        builder.appendQueryParameter("cvar", cvar.toString());
+    }
+
+    private String md5(final String s)
+            throws NoSuchAlgorithmException {
+        MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+        digest.update(s.getBytes());
+        byte messageDigest[] = digest.digest();
+
+        // Create Hex String
+        StringBuilder hexString = new StringBuilder();
+        for (byte aMessageDigest : messageDigest) {
+            String h = Integer.toHexString(0xFF & aMessageDigest);
+            while (h.length() < 2)
+                h = "0" + h;
+            hexString.append(h);
+        }
+        return hexString.toString();
+    }
+
+    private HttpHost getProxy(Context context) {
+        String proxyAddress;
+        int proxyPort;
+
+        if (IS_ICS_OR_LATER) {
+            proxyAddress = System.getProperty( "http.proxyHost" );
+
+            String portStr = System.getProperty( "http.proxyPort" );
+            proxyPort = Integer.parseInt( ( portStr != null ? portStr : "-1" ) );
+        } else {
+            proxyAddress = android.net.Proxy.getHost( context );
+            proxyPort = android.net.Proxy.getPort( context );
+        }
+
+        if (proxyAddress == null)
+            return null;
+        return new HttpHost(proxyAddress, proxyPort);
+    }
+
+    class AsyncTrack implements Runnable {
+        private TVSquaredCollector tracker;
+        private String actionname;
+        private String product;
+        private String promocode;
+        private float revenue;
+        private String orderid;
+
+        public AsyncTrack(TVSquaredCollector tracker, String actionname, String product, String orderid, float revenue, String promocode) {
+            this.tracker = tracker;
+            this.actionname = actionname;
+            this.product = product;
+            this.promocode = promocode;
+            this.revenue = revenue;
+            this.orderid = orderid;
+        }
+
+        public void run() {
+            try {
+                Uri.Builder builder = new Uri.Builder();
+                builder.scheme(this.tracker.secure ? "https" : "http")
+                        .authority(this.tracker.hostname)
+                        .path("/piwik/piwik.php")
+                        .appendQueryParameter("idsite", String.valueOf(this.tracker.siteid))
+                        .appendQueryParameter("rec", "1")
+                        .appendQueryParameter("rand", "" + String.valueOf(random.nextInt()))
+                        .appendQueryParameter("_id", this.tracker.visitorid);
+                this.tracker.appendSessionDetails(builder);
+                if ((actionname != null) && (actionname.trim().length() > 0))
+                    this.tracker.appendActionDetails(builder, actionname, product, promocode, revenue, orderid);
+
+               HttpClient client = new DefaultHttpClient();
+               if (this.tracker.proxy != null)
+                   client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, this.tracker.proxy);
+               HttpGet request = new HttpGet(builder.build().toString());
+               request.setHeader("User-Agent", "TVSquared Android Collector Client 1.0");
+
+               HttpResponse resp = client.execute(request);
+               if (resp.getStatusLine().getStatusCode() != 200)
+                   throw new RuntimeException("Failed to track request: " + resp.getStatusLine().toString());
+            } catch (JSONException ex) {
+                throw new RuntimeException(ex.toString(), ex);
+            } catch (ClientProtocolException ex) {
+                throw new RuntimeException(ex.toString(), ex);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex.toString(), ex);
+            }
+        }
+    }
+}
---
+++ src/com/tvsquared/sample/SampleActivity.java
@@ -0,0 +1,63 @@
+package com.tvsquared.sample;
+
+import java.security.NoSuchAlgorithmException;
+
+import com.tvsquared.TVSquaredCollector;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class SampleActivity extends Activity {
+
+    private TVSquaredCollector tracker = null;
+    private EditText txt_userid;
+    private EditText txt_actionname;
+    private EditText txt_product;
+    private EditText txt_orderid;
+    private EditText txt_revenue;
+    private EditText txt_promocode;
+
+    public SampleActivity() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            this.tracker = new TVSquaredCollector(this, "collector.tvsquared.com", "1");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        setContentView(R.layout.sample);
+        txt_userid = (EditText)findViewById(R.id.txt_userid);
+        txt_actionname = (EditText)findViewById(R.id.txt_actionname);
+        txt_product = (EditText)findViewById(R.id.txt_product);
+        txt_orderid = (EditText)findViewById(R.id.txt_orderid);
+        txt_revenue = (EditText)findViewById(R.id.txt_revenue);
+        txt_promocode = (EditText)findViewById(R.id.txt_promocode);
+    }
+
+    public void trackSimple(View view) {
+        if (this.tracker != null)
+            this.tracker.track();
+    }
+
+    public void trackUser(View view) {
+        if (this.tracker != null) {
+            this.tracker.setUserId(txt_userid.getText().toString());
+            this.tracker.track();
+        }
+    }
+
+    public void trackAction(View view) {
+        if (this.tracker != null) {
+            this.tracker.setUserId(txt_userid.getText().toString());
+            this.tracker.track(txt_actionname.getText().toString(), txt_product.getText().toString(), txt_orderid.getText().toString(), Float.parseFloat(txt_revenue.getText().toString()), txt_promocode.getText().toString());
+        }
+    }
+}
